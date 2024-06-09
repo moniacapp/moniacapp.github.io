856a79df3e6cabf0119a258d7706716bb247c3dd
moniacapp <moneytracker.development@gmail.com>
2024-06-09 16:20:25 +0800

    Working on ContactUs page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -3,31 +3,66 @@ import './ContactUs.css';
 import Header from '../components/Header';
 
 class ContactUs extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      email: ''
+    };
+  }
+
+  onTextInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  onSubmitClick = (event) => {
+    event.preventDefault();
+
+    console.warn('Sending email:', this.state);
+
+    const templateParams = {
+      message: this.state.message,
+      email: this.state.email
+    };
+
+    this.setState({
+      message: '',
+      email: ''
+    });
+  };
+
   render() {
     return (
       <>
         <Header />
         <div className="contact-us">
-          <h1>System Color Picker</h1>
-          <h2>Feedback & Support</h2>
-          <p>
-            If you have a GitHub account, <a href="https://github.com" target="_blank" rel="noopener noreferrer">open an issue on the repo</a> instead.
-          </p>
+          <h1>Feedback & Support</h1>
           <form>
             <label>
               Message*
-              <textarea name="message" placeholder="I'm a human. Please be nice." />
+              <textarea
+                name="message"
+                placeholder="I'm a human. Please be nice."
+                value={this.state.message}
+                onChange={this.onTextInputChange}
+              />
             </label>
             <label>
               Email*
-              <input type="email" name="email" placeholder="Your email" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Your email"
+                value={this.state.email}
+                onChange={this.onTextInputChange}
+              />
             </label>
             <small>Only used for replying to you.</small>
-            <label>
-              Attachments
-              <input type="file" name="attachments" multiple />
-            </label>
-            <button type="submit">Send Feedback</button>
+            <button type="submit" onClick={this.onSubmitClick}>Send Feedback</button>
           </form>
         </div>
       </>
